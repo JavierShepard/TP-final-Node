e5f9c1ac738aff7b1788b5424183f8f07b232887
JavierShepard <comander.javier@gmail.com>
2024-07-12 21:36:47 -0300

    v4

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
